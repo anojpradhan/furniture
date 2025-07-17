740872da39f30349498cc38a0ab67043f17fd043
anoz05 <159677775+anojpradhan@users.noreply.github.com>
2025-07-17 16:13:39 +0545

    dropdown border change

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -18,7 +18,7 @@ const Dropdown = ({ lists }) => {
       className="relative group inline-block mt-2 w-full bg-white text-gray-900 cursor-pointer"
       onClick={() => navigate(`/category/${createSlug(lists.title)}`)}
     >
-      <div className="w-full flex justify-center border-b-2 border-gray">
+      <div className="w-full flex justify-center border-b-2 border-gray-300">
       <button
         type="button"
         className=" group relative py-2 text-center font-medium capitalize hover:text-blue-800 transition-all duration-300"
@@ -32,14 +32,14 @@ const Dropdown = ({ lists }) => {
       <div
         role="menu"
         aria-orientation="vertical"
-        className="absolute left-2 flex flex-wrap mt-1 w-md bg-white border border-blue-300 rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-200 z-20"
+        className="absolute left-0 top-full flex flex-wrap mt-1 w-full bg-white border border-blue-300 rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-200 z-20"
       >
         {lists.types.map((type, i) => (
           <div
             role="menuitem"
             tabIndex={-1}
             key={i}
-            className="px-4 py-2 text-left w-1/3 text-gray-900 hover:bg-blue-600 capitalize hover:text-white transition cursor-pointer rounded-md"
+            className="px-4 py-2 text-left w-xs text-gray-900 hover:bg-blue-600 capitalize hover:text-white transition cursor-pointer rounded-md"
             onClick={(e) => handleTypeClick(type, e)}
           >
             {type}
